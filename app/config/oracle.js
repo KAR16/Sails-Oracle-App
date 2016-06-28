495c28cfad9d499c4d6e6397f25a977d1f8785e3
KAR16 <kherrera16@gmail.com>
2016-06-28 15:14:57 -0600

    Agregamos la carpeta del proyecto

---
+++ app/config/oracle.js
@@ -0,0 +1,5 @@
+module.exports.oracle = {
+    "user" : "<<USER>>",
+    "password" : "<<PASSWORD>>",
+    "tns" : "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = <<0.0.0.0>>)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = <<SERVICE_NAME>>)))"
+};
